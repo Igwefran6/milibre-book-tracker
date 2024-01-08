13f2f8b1514f272ad76d6ed511113efaac0117ab
igwe francis <igwefran6@gmail.com>
2024-01-08 15:51:07 +0100

    replaced dialog button tag with input because button tag couldn't trigger required. Removed unnecessary variables and more

--- js/index.js
+++ js/index.js
@@ -1,37 +1,58 @@
 // Defining variables
 
 const dialog = document.querySelector(".dialog");
-const addNewBook = document.querySelector(".add-new-book");
-const cancel = document.querySelector(".cancel");
-
-const toggleNav = document.querySelector(".toggle-nav");
 
 const navigation = document.querySelector(".navigation");
-
-let opened;
 
 /*Manipulation of the UI, such as making the dislog show and close, making the
 toggle button to open navigation etc
 */
 
-addNewBook.addEventListener("click", () => {
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    static setBookToLocalStorage() {
+        localStorage.setItem("book", "work book set");
+    }
+
+    static getBookFromLocalStorage() {
+        return localStorage.getItem("book");
+    }
+
+    static displayBooks() {
+        this.setBookToLocalStorage();
+        console.log((this.title = title));
+    }
+}
+
+// Necessary Events
+document.querySelector(".ok").addEventListener("click", event => {
+    // event.preventDefault();
+    Book.displayBooks();
+    dialog.close();
+});
+
+document.querySelector(".add-new-book").addEventListener("click", () => {
     dialog.showModal();
 });
 
-cancel.addEventListener("click", event => {
+document.querySelector(".cancel").addEventListener("click", event => {
     event.preventDefault();
     dialog.close();
 });
 
-toggleNav.addEventListener("click", () => {
+document.querySelector(".toggle-nav").addEventListener("click", () => {
     navigation.classList.toggle("open");
 });
 
-class Book{
-  constructor(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read;
-  }
-}
+let title = document.querySelector("#book-title").value,
+    author = document.querySelector("#book-author").value,
+    pages = document.querySelector("#book-page-count").value,
+    isRead = document.querySelector("#isRead").checked;
+
+let book = new Book(title, author, pages, isRead);
